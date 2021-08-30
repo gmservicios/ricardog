a356f6a6f7c15de696862fa800ce5d1f8fa9c800
gmservicios <75394149+gmservicios@users.noreply.github.com>
2021-08-30 08:32:47 -0600

    Delete sales_warehouses_stock directory

--- sales_warehouses_stock/models/stock_warehouse.py
+++
@@ -1,6 +0,0 @@
-from odoo import _, api, fields, models
-
-class Warehouse(models.Model):
-    _inherit = "stock.warehouse"
-
-    visible_on_sol = fields.Boolean(string="Visible on Sale order line", default=True)
